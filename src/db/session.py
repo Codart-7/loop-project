5370f80031ae909577a5a51483b74d24ed8544a1
Codart-7 <iconmoa@gmail.com>
2023-04-28 10:04:44 +0100

    general update

--- src/db/session.py
+++ src/db/session.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from typing import Generator
 
 from config.config import settings
 
@@ -7,4 +8,13 @@ from config.config import settings
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
+
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+
+def get_db() -> Generator:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- src/routes/router.py
+++ src/routes/router.py
@@ -1,14 +1,16 @@
-from fastapi import APIRouter, Request, Response, Body
+from fastapi import APIRouter
+
+from schema.message import Message
 
 
 end_points = APIRouter()
 
 
 @end_points.get("/trigger_report")
-async def trigger_report(request: Request):
-	return {"message": "Report"} 
+async def trigger_report():
+	return "Report"
 
 
 @end_points.get("/get_report")
-async def get_report(reportID: Response):
-	return reportID.body
+async def get_report(reportID: Message):
+	return reportID.report_id
